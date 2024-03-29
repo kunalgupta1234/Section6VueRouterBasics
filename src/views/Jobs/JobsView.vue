5134f373e2d17f8bda1c86ecc0c686b3469f5c1f
KunalGupta <g.kunal9@gmail.com>
2024-03-29 14:48:23 +0530

    55-where to fetch data

--- src/views/Jobs/JobsView.vue
+++ src/views/Jobs/JobsView.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>Jobs</h1>
   <div v-for="job in jobs" :key="job.id" class="job">
-    <router-link :to="{name:'JobDetails', params:{id:job.id}}">
+    <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
       <h2>{{ job.title }}</h2>
     </router-link>
   </div>
@@ -11,30 +11,32 @@
 export default {
   data() {
     return {
-      jobs: [
-        { title: "job1", id: 1, details: "lorem" },
-        { title: "job2", id: 2, details: "lorem" },
-        { title: "job3", id: 3, details: "lorem" },
-      ],
+      jobs: [],
     };
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs")
+      .then((res) => res.json())
+      .then((data) => (this.jobs = data))
+      .catch((err) => console.log(err.message));
   },
 };
 </script>
 
 <style>
-  .job h2{
-    background: #f4f4f4;
-    padding: 20px;
-    border-radius: 10px;
-    margin: 10px auto;
-    max-width: 600px;
-    cursor: pointer;
-    color: #444;
-  }
-  .job h2:hover {
-    background: #ddd;
-  }
-  .job a{
-    text-decoration: none;
-  }
+.job h2 {
+  background: #f4f4f4;
+  padding: 20px;
+  border-radius: 10px;
+  margin: 10px auto;
+  max-width: 600px;
+  cursor: pointer;
+  color: #444;
+}
+.job h2:hover {
+  background: #ddd;
+}
+.job a {
+  text-decoration: none;
+}
 </style>
